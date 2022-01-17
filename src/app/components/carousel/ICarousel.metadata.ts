5299912b7e79c29a4f0d041ff08f338b0dcd205e
Oscar Dominguez <oscardominguezn.n@outlook.com>
2022-01-17 12:52:44 -0600

    se agrego la interface del componente

---
+++ src/app/components/carousel/ICarousel.metadata.ts
@@ -0,0 +1,13 @@
+export interface ICarousel {
+    id: number;
+    title?: {
+        first: string;
+        second: string;
+    };
+    subtitle?: string;
+    link?: string;
+    image: string;
+    order?: number;
+    marginLeft?: number;
+    time?: number;
+}
